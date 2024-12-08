4620f960c2e4803c9927c9afa4c88a6deba30753
Hestefane Cardoso <hestefanecardoso@gmail.com>
2024-12-08 11:11:03 -0300

    Feature: added subtraction

---
+++ calculadora.js
@@ -0,0 +1,24 @@
+const args = process.argv.slice(2);
+console.log(parseInt(args[0]) + parseInt(args[1]));
+const soma = () => {
+    console.log(parseInt(args[0]) + parseInt(args[1]));
+};
+
+const sub = () => {
+    console.log(parseInt(args[0]) - parseInt(args[1]));
+}
+
+const args = process.argv.slice(2);
+
+switch (args[0]) {
+    case 'soma':
+        soma();
+    break;
+
+    case 'sub':
+        sub();
+    break;
+
+    default:
+        console.log('does not support', arg[0]);
+}
