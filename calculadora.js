98e7bf8f7165c5ce4a1e0092bdbfa4b2dbb5600b
Hestefane Cardoso <hestefanecardoso@gmail.com>
2024-12-08 11:28:49 -0300

    Corrigi: erro na declaração da variavel args repetido, erro no switch e erro na linha 23.

--- calculadora.js
+++ calculadora.js
@@ -1,24 +1,23 @@
-const args = process.argv.slice(2);
-console.log(parseInt(args[0]) + parseInt(args[1]));
+const args = process.argv.slice(2); 
+
 const soma = () => {
-    console.log(parseInt(args[0]) + parseInt(args[1]));
+    console.log(parseInt(args[1]) + parseInt(args[2]));
 };
 
-const sub = () => {
-    console.log(parseInt(args[0]) - parseInt(args[1]));
-}
 
-const args = process.argv.slice(2);
+const sub = () => {
+    console.log(parseInt(args[1]) - parseInt(args[2]));
+};
 
 switch (args[0]) {
     case 'soma':
         soma();
-    break;
+        break;
 
     case 'sub':
         sub();
-    break;
+        break;
 
     default:
-        console.log('does not support', arg[0]);
+        console.log('does not support', args[0]);
 }
