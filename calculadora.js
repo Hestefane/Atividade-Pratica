2c51540454b1b4aa4793f426ebced3dae2a7332e
Hestefane Cardoso <hestefanecardoso@gmail.com>
2024-12-08 12:21:27 -0300

    feat: add conditional evaluation

--- calculadora.js
+++ calculadora.js
@@ -1,5 +1,12 @@
 const args = process.argv.slice(2);
+var x = args[0];
+var y = args[2];
+var operator = args[1];
+function evaluate(param1, param2, operator) {
+return eval(param1 + operator + param2);
 
+}
+if ( console.log( evaluate(x, y, operator) ) ) {}
 const soma = () => {
     console.log(parseInt(args[1]) + parseInt(args[2]));
 };
