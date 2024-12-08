40ed4e96fe013838a3ddcb66e157cf3b1d1a582d
Hestefane Cardoso <hestefanecardoso@gmail.com>
2024-12-08 12:04:04 -0300

    Nova funcionalidade de divisão na calculadora

--- calculadora.js
+++ calculadora.js
@@ -1,12 +1,20 @@
-const args = process.argv.slice(2); 
+const args = process.argv.slice(2);
 
 const soma = () => {
     console.log(parseInt(args[1]) + parseInt(args[2]));
 };
 
-
 const sub = () => {
     console.log(parseInt(args[1]) - parseInt(args[2]));
+};
+
+const div = () => {
+    const divisor = parseInt(args[2]);
+    if (divisor === 0) {
+        console.log("Erro: Não é permitido divisão por zero.");
+    } else {
+        console.log(parseInt(args[1]) / divisor);
+    }
 };
 
 switch (args[0]) {
@@ -16,6 +24,10 @@ switch (args[0]) {
 
     case 'sub':
         sub();
+        break;
+
+    case 'div':
+        div();
         break;
 
     default:
